6f465dcea3b4c20f3d87c33cfb491975ba9c0458
JulianCamiloLG <julycamy_125@hotmail.com>
2018-02-04 22:31:43 -0500

    archivo JSON

--- src/logica/LogicaJSON.java
+++ src/logica/LogicaJSON.java
@@ -9,6 +9,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Set;
 import javax.json.*;
 import javax.json.stream.JsonParser;
 
@@ -29,24 +32,25 @@ public class LogicaJSON{
         this.route = route;
     }
     
-    public void AbrirJSON() throws FileNotFoundException{
+    public LinkedHashMap AbrirJSON() throws FileNotFoundException{
+        LinkedHashMap<String, LinkedList<String[]>> infoMinasCompleta= new LinkedHashMap<>();
         File archivo = new File(route);
         InputStream lector = new FileInputStream(archivo);
-        JsonReader reader = Json.createReader(lector);
-        System.out.println(reader);
-        JsonArray objetojson =  reader.readArray();
-        System.out.println(objetojson);
-        JsonObject objetito = objetojson.getJsonObject(1);
-        JsonObject objetito2 =objetojson.getJsonObject(2);
-        JsonArray objeto2 = objetito2.getJsonArray("minas");
-        JsonObject objeto3= objeto2.getJsonObject(0);
-        JsonObject objeto4 = objeto3.getJsonObject("0");
-        System.out.println(objetito);
-        for (JsonValue string : objeto4.values()) {
-            System.out.println(string);
-        }
-        System.out.println(objeto3.values());
-        System.out.println(objeto4.getJsonArray("seccionesMina"));
+        JsonReader lectorJson = Json.createReader(lector);
+        JsonArray arregloCompleto =  lectorJson.readArray();
+        extraerInfoMineros(arregloCompleto.getJsonObject(0));
+        infoMinasCompleta=extraerMinas(arregloCompleto.getJsonObject(1));
+        return infoMinasCompleta;
+    }
+
+    private void extraerInfoMineros(JsonObject mineros) {
+        System.out.println(mineros);
+        JsonObject minerosCompleto=mineros.getJsonObject("infomineros");
+        System.out.println(minerosCompleto);
+    }
+
+    private LinkedHashMap<String, LinkedList<String[]>> extraerMinas(JsonObject jsonObject) {
+        return null;
     }
     
 }
